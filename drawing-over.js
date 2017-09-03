84781b77fdf8d0cfc3b114ef3e8ac83f7db1e0d1
Anwesh43 <anweshthecool0@gmail.com>
2017-09-03 16:52:42 +0530

    adding Shape and point class

--- drawing-over.js
+++ drawing-over.js
@@ -26,3 +26,29 @@ class DrawingOverScreen {
         context.fillRect(0,0,canvas.width,canvas.height)
     }
 }
+class Shape {
+    constructor() {
+        this.points = []
+    }
+    draw(context) {
+        context.beginPath()
+        this.points.forEach((point,index)=>{
+            if(index == 0) {
+                context.moveTo(point.x,point.y)
+            }
+            else {
+                context.lineTo(point.x,point.y)
+            }
+        })
+        context.stroke()
+    }
+    addPoint(x,y) {
+        this.points.push(new Point(x,y))
+    }
+}
+class Point {
+    constructor(x,y) {
+        this.x = x
+        this.y = y
+    }
+}
